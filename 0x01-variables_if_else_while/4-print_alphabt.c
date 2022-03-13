14c350e8b9ec512af8cb08c3d9f2256d42170c55
knife-co <natomiwad@gmail.com>
2022-03-13 14:21:41 -0700

    skin

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char a = 'a';
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if ((a != 'e') && (a != 'q'))
+		{
+			putchar(a);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
