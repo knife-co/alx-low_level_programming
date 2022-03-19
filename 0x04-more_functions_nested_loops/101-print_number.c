190ac70859156121636ed989d03bda087baf5c9d
knife-co <natomiwad@gmail.com>
2022-03-19 15:26:59 -0700

    goal

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,44 +1,21 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main - print.
+ * print_number - print integer.
+ * @n: integer.
  *
- * Return: Always 0 (Success)
+ * Return: void.
  */
-int main(void)
+void print_number(int n)
 {
-	int i, e, g;
+	unsigned int x;
 
-	i = 48;
-	e = 48;
-	g = 48;
-
-	while (e < 58)
+	x = n;
+	if (n < 0)
 	{
-		i = 48;
-		while (i < 58)
-		{
-			g = 48;
-			while (g < 58)
-			{
-				if (e != i && e != g && i != g && e < i && i < g)
-				{
-					putchar(e);
-					putchar(i);
-					putchar(g);
-					if (i == 56 && e == 55 && g == 57)
-					{
-						break;
-					}
-					putchar(',');
-					putchar(' ');
-				}
-				g++;
-			}
-			i++;
-		}
-		e++;
+		_putchar(45);
+		x = -n;
 	}
-	putchar('\n');
-	return (0);
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
